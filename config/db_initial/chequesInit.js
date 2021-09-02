37ddc3ee1b654934b32c5f9c3972ad23ec3aa2cc
marcoref3at <marco_refaat@hotmail.com>
2021-09-02 15:41:30 +0200

    cheques error

--- config/db_initial/chequesInit.js
+++ config/db_initial/chequesInit.js
@@ -3,19 +3,18 @@ const { cheques } = sequelize.models;
 const _CHEQUES = require("../../_data/_cheques.json");
 
 const chequesInit = async () => {
-  setTimeout(() => {
-    cheques
-      .bulkCreate(_CHEQUES, {
-        validate: true,
-        individualHooks: true
-      })
-      .then(async () => {
-        const count = await cheques.count();
-        console.log("cheques created:".bgYellow.black.bold, count);
-      })
-      .catch(e => {
-        console.log("cheques initializing error".red.bold);
-      });
-  }, 900);
+  cheques
+    .bulkCreate(_CHEQUES, {
+      validate: true,
+      individualHooks: true
+    })
+    .then(async () => {
+      const count = await cheques.count();
+      console.log("cheques created:".bgYellow.black.bold, count);
+    })
+    .catch(e => {
+      console.log("e:", e);
+      console.log("cheques initializing error".red.bold);
+    });
 };
 module.exports = chequesInit;

4753469c1ca31108c12df8d87c89198301a3928c
marcoref3at <marco_refaat@hotmail.com>
2021-09-08 03:19:52 +0200

    refactoring

--- controllers/cheques/_functions.js
+++ controllers/cheques/_functions.js
@@ -14,70 +14,41 @@ const includeDevices = {
   ],
 };
 
-// Filter Table Records by Ids
-const getIds = async (TableName, names) => {
-  let Ids = [];
-  await Promise.all(
-    names.map(async (name) => {
-      // Get Permissions of target user role and all children roles
-      let record = await sequelize.models[TableName].findOne({
-        where: {
-          name,
-        },
-      });
-      //To Handle misspelled permission names
-      //And Repeated Permissions
-      if (record && !Ids.includes(record.dataValues.id)) {
-        return Ids.push(record.dataValues.id);
-      }
-    })
-  );
-  return Ids;
-};
+const getOrdersTotalPrice = (orderz) => {
+  // Get total quanitity of each id
+  const lookup = orderz.reduce((a, e) => {
+    a[e.itemId] = a[e.itemId] + e.quantity || e.quantity;
+    return a;
+  }, {});
 
-// Filter Table Records by names
-const getNames = async (TableName, Ids) => {
-  let names = [];
-  await Promise.all(
-    Ids.map(async (id) => {
-      let record = await sequelize.models[TableName].findOne({
-        where: { id },
-      });
-      //To Handle misspelled record names
-      //And Repeated Names
-      if (record && !names.includes(record.dataValues.name)) {
-        return names.push(record.dataValues.name);
-      }
-    })
-  );
-  return names;
-};
-
-// 1- Get Features Ids
-// 2-Get Features Names
-// 3-Associate features ids to the target device
-// 4-Return Back the new device data
-const setDevices = async (devicesUnfilteredNames, device) => {
-  console.log("devicesUnfilteredNames:", devicesUnfilteredNames);
-  // Filter Features by name and Ids
-  //1- Get Features IDs by filtering them
-  let devicesIds = await getIds("Devices", devicesUnfilteredNames);
-
-  // 2-Get Names of the previous filtered features
-  let devicesNames = await getNames("Devices", devicesIds);
-
-  return new Promise(async (resolve, reject) => {
-    await device.setDevices([...devicesIds]);
-    // Add Filtered Features to response
-    device.dataValues.zone = devicesNames;
-    resolve(device); //returns x in .then
+  // Modify quantity of each id
+  let modifiedQuantities = orderz.filter((e) => {
+    e.quantity = lookup[e.itemId];
+    return lookup[e.itemId];
   });
+
+  // Remove duplicates from array
+
+  let filtered = Object.values(
+    modifiedQuantities.reduce(
+      (acc, cur) => Object.assign(acc, { [cur.itemId]: cur }),
+      {}
+    )
+  );
+
+  filtered.forEach(
+    (item) => (item.dataValues.totalPrice = item.quantity * item.unitPrice)
+  );
+
+  // Get Cheque Total price
+  let total = filtered.reduce(
+    (acc, curr) => acc.dataValues.totalPrice + curr.dataValues.totalPrice
+  );
+  return total;
 };
 
 module.exports = {
   execludeAttribute,
   includeDevices,
-  getIds,
-  getNames,
-  setDevices,
+  getOrdersTotalPrice,
 };
--- controllers/cheques/getCheque.js
+++ controllers/cheques/getCheque.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("../../middleware/async");
 const sequelize = require("../../sequelize");
-const { execludeAttribute, includeDevices } = require("./_functions");
+const { getOrdersTotalPrice } = require("./_functions");
 const { cheques, orders } = sequelize.models;
 
 // Get Cheque status (isVoid,isClosed,tableId)
@@ -10,12 +10,11 @@ exports.getCheque = asyncHandler(async (req, res, next) => {
   // Get All Cheques
   if (req.params.id == 0) {
     cheque = await cheques.findAll();
-    // Count Retrieved Devices
+    // Count Retrieved Cheques
     count = await cheques.count();
   } else {
     // Get one cheque
     cheque = await cheques.findByPk(req.params.id);
-    // Count Retrieved Devices
     count = 1;
   }
   res.status(200).json({ success: true, count, data: cheque });
@@ -30,40 +29,7 @@ exports.getChequeOrders = asyncHandler(async (req, res, next) => {
     },
   });
 
-  total = sumUpQuantitiesAndPrice(orderz);
+  total = getOrdersTotalPrice(orderz);
 
   res.status(200).json({ success: true, total, data: orderz });
 });
-
-const sumUpQuantitiesAndPrice = (orderz) => {
-  // Get total quanitity of each id
-  const lookup = orderz.reduce((a, e) => {
-    a[e.itemId] = a[e.itemId] + e.quantity || e.quantity;
-    return a;
-  }, {});
-
-  // Modify quantity of each id
-  let modifiedQuantities = orderz.filter((e) => {
-    e.quantity = lookup[e.itemId];
-    return lookup[e.itemId];
-  });
-
-  // Remove duplicates from array
-
-  let filtered = Object.values(
-    modifiedQuantities.reduce(
-      (acc, cur) => Object.assign(acc, { [cur.itemId]: cur }),
-      {}
-    )
-  );
-
-  filtered.forEach(
-    (item) => (item.dataValues.totalPrice = item.quantity * item.unitPrice)
-  );
-
-  // Get Cheque Total price
-  let total = filtered.reduce(
-    (acc, curr) => acc.dataValues.totalPrice + curr.dataValues.totalPrice
-  );
-  return total;
-};
